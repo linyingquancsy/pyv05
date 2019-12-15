c46128ba7c8dd3802f15add0316216b311dbcb91
linyingquan <18282513893@163.com>
2019-12-15 22:55:04 +0800

    py05

--- pyv05/settings.py
+++ pyv05/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=k=)jadp@ii3hr^uuav&*&#p6p!c&ryd4^xv1gid%_^3h&gmpx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
